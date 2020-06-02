d9f8a220302ac3f8228a3eb4673a3b85fc7cb674
kondetisidhartha <kondetisidhartha@gmail.com>
2020-06-02 21:20:51 +0200

    created pages and added browserrouter to index.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Cart from './pages/CartPage';
+import Contact from './pages/ContactPage';
+import Default from './pages/Default';
+import Products from './pages/ProductsPage';
+import SingleProduct from './pages/SingleProductPage';
+
 function App() {
   return (
     <h1>Hello from Tech Store</h1>
